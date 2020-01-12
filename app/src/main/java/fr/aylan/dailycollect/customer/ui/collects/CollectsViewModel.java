cc2ce54fa18a99c299457edbded8e1818f131564
Ismail Koksal <ismail@koksal.fr>
2020-01-12 19:42:25 +0100

    Created the customer activity

---
+++ app/src/main/java/fr/aylan/dailycollect/customer/ui/collects/CollectsViewModel.java
@@ -0,0 +1,7 @@
+package fr.aylan.dailycollect.customer.ui.collects;
+
+import androidx.lifecycle.ViewModel;
+
+public class CollectsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/fr/aylan/dailycollect/customer/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package fr.aylan.dailycollect.customer.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
