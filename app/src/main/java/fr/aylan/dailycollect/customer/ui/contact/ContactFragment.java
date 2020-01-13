90b0192c6b359033e8a86057d630862d8b8f0300
Ismail Koksal <ismail@koksal.fr>
2020-01-13 11:24:10 +0100

    Contact view : - UI - Compose email (open email app)

---
+++ app/src/main/java/fr/aylan/dailycollect/customer/ui/contact/ContactFragment.java
@@ -0,0 +1,79 @@
+package fr.aylan.dailycollect.customer.ui.contact;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import fr.aylan.dailycollect.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContactFragment extends Fragment {
+
+    private EditText nameField;
+    private EditText emailField;
+    private EditText subjectField;
+    private EditText messageField;
+    private Button sendEmailButton;
+
+    public ContactFragment() {
+        // Required empty public constructor
+    }
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Views
+        nameField = view.findViewById(R.id.fieldName);
+        emailField = view.findViewById(R.id.fieldEmail);
+        subjectField = view.findViewById(R.id.fieldSubject);
+        messageField = view.findViewById(R.id.fieldMessage);
+        sendEmailButton = view.findViewById(R.id.sendEmailButton);
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeEmail(emailField.getText().toString(),
+                        subjectField.getText().toString(),
+                        messageField.getText().toString()
+                );
+            }
+        });
+    }
+
+    private void composeEmail(String email, String subject, String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+}
